f2cd6aed417f125aa6f02dc075f041808e9f94e9
Rupesh Virani <rupeshvirani01@gmail.com>
2024-04-18 17:36:11 +0530

    make view for GET POST Todolist API

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.response import Responses
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import Todo
+from .serializers import TodoSerializer
 
 # Create your views here.
+
+@api_view(["GET","POST"])
+def todo_list(request):
+    if request.method == "GET":
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos,many = True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Responses(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
