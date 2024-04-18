300ea543ca327e35c5e0c44fe3876b13388f1584
Rupesh Virani <rupeshvirani01@gmail.com>
2024-04-18 18:32:26 +0530

    add URL path(API end Point)

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.response import Responses
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from .models import Todo
